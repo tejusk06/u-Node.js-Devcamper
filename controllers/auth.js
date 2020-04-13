38e7e30874bc7214a25ec9c7838ccd20b75ca165
Tejus <apple@Admins-MacBook-Pro.local>
2020-04-14 05:14:23 +0530

    done 47

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,23 @@ const asyncHandler = require('../middleware/async');
 // @route     GET /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({
-    success: true
+  const { name, email, password, role } = req.body;
+  //? Pulling the fields out because the password needs to be hashed in middleware
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
   });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+  //? call the '.getSignedJwtToken' on 'user' instance not 'User' model
+
+  res.status(200).json({
+    success: true,
+    token
+  })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -32,5 +34,22 @@ const UserSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  //? Higher the gensalt number the more secure but also heavier on your system. 10 is recommended
+  this.password = await bcrypt.hash(this.password, salt);
+  console.log();
+
+})
+
+// Sign JWT and return 
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  })
+  //? 'this._id' gives the specific users 'id'. Since this is a method and not a static it will be called on the particular user and not the User model.
+}
 
 module.exports = mongoose.model('User', UserSchema);
