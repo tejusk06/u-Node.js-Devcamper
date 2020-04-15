3b1952f9eb132574a267f6bf989042e2a6b12f22
Tejus <apple@Admins-MacBook-Pro.local>
2020-04-15 10:09:16 +0530

    done 55

--- controllers/courses.js
+++ controllers/courses.js
@@ -53,12 +53,20 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 exports.addCourse = asyncHandler(async (req, res, next) => {
 
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+  //? We have req.user available because of protect middleware
 
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
     return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404);
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`, 401));
+  }
+
 
   const course = await Course.create(req.body);
 
@@ -79,6 +87,11 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to update course ${course._id}`, 401));
   }
 
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
@@ -103,6 +116,11 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete course ${course._id}`, 401));
   }
 
   await course.remove();
