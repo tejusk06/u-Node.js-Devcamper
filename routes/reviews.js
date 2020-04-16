56264fb25bbacf849088f55c27e919b94234b789
Tejus <apple@Admins-MacBook-Pro.local>
2020-04-16 08:22:23 +0530

    done 62

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
@@ -16,5 +16,10 @@ router
       select: 'name description'
     }), getReviews
   )
+
+
+router
+  .route('/:id')
+  .get(getReview)
 
 module.exports = router;
