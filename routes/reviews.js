68283c0d7e863a8f53b89eb624bcc4223ae043e3
Tejus <apple@Admins-MacBook-Pro.local>
2020-04-16 09:12:55 +0530

    done 63

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getReviews, getReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
@@ -16,6 +16,7 @@ router
       select: 'name description'
     }), getReviews
   )
+  .post(protect, authorize('user', 'admins'), addReview)
 
 
 router
