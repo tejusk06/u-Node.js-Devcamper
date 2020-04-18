6cabe2d53ca7fc2ac54ecf3b15ee3aaadac5d0e7
Tejus <apple@Admins-MacBook-Pro.local>
2020-04-19 03:03:42 +0530

    done 68

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const colors = require('colors');
 const path = require('path');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
+const xss = require('xss-clean')
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -37,6 +40,15 @@ if (process.env.NODE_ENV === 'development') {
 // File uploading 
 //? Enables express-fileupload
 app.use(fileupload());
+
+// Sanitize data 
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks 
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
